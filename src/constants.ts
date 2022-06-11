1e7625543740eba344956683abd0a1b7fc7aa97a
Suraj Lama <suraj8848mt@gmail.com>
2022-06-12 02:17:28 +0545

    modified

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,6 @@ export const INSTANT_SEARCH_QUERY_SUGGESTIONS =
   'articles_query_suggestions';
 export const INSTANT_SEARCH_HIERARCHICAL_ATTRIBUTES = [
   'category_indexing',
-  'category_child_indexing'
 
 ];
 
